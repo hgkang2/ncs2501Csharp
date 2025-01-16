135d007b62cebaf92c103e486074ba143fa43741
hgkang2 <hgkang2hank@naver.com>
2025-01-16 15:29:26 +0900

    편지,배열나누기

--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@
     private static void Main(string[] args)
     {
         Solution sol = new Solution();
-        int[] intarray = new int[]{1,2,3,4,5,6,7,8,9,10};
-        //Console.WriteLine(sol.solution0115(intarray));
-       Sample sam = new Sample();
-        sam.Queue();
+        int[] intarray = new int[]{1,2,3,4,5};
+        string str = "happy birthday!";
+        //Console.WriteLine(sol.solution01162(intarray,1,3));
+
+        Util.printIntArray(sol.solution01162(intarray,1,3));
+      /* Sample sam = new Sample();
+        sam.Queue();*/
     }
 }
