8dbe5d99879d5d932d06329d014a6d2cf6e616dc
hgkang2 <hgkang2hank@naver.com>
2025-01-21 10:33:15 +0900

    string

--- Sample.cs
+++ Sample.cs
@@ -4,10 +4,35 @@ using System.Data;
 using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Marshalling;
+using System.Text;
 
 
 class Sample
 {
+    public void stringBuilderSample()
+    {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= 26; i++)
+        {
+            sb.Append(i.ToString());
+            sb.Append(System.Environment.NewLine);
+        }
+        string s = sb.ToString();
+        Console.WriteLine(s);
+    }
+    public void stringSample()
+    {
+        string s1 = "C#";
+        string s2 = "programming";
+        char c1 = 'A';
+        char c2 = 'B';
+
+        string s3 = s1 + " "+ s2;
+        Console.WriteLine("String: {0}" , s3);
+
+        string s3substring = s3.Substring(1,5);
+        Console.WriteLine("Substring: {0}",s3substring);
+    }
     public void Dictionary()
     {
 
